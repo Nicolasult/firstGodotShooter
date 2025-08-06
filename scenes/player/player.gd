ab1c857b019df9defffdd8375947809edf7fb4c5
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-06 23:13:28 +0200

    basic movement and shooting triggers witj keys

---
+++ scenes/player/player.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+func _process(delta):
+	
+	# input
+	var direction = Input.get_vector("left", "right", "up", "down")
+	position += direction * 500 * delta
+	
+	# laser shooting input
+	if Input.is_action_just_pressed("primary action"):
+		print("Shoot laser")
+		
+	# grenade shooting input
+	if Input.is_action_just_pressed("secondary action"):
+		print("Shoot grenade")
