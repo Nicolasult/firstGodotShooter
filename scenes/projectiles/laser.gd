23d0b6c7f9d5c7290cbdf7b8641102aea6bae321
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-10 11:54:29 +0200

    basic inheritance setup for levels

--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var speed: int = 1000
+@export var speed: int = 1800
 var direction: Vector2 = Vector2.UP
 
 func _process(delta):
@@ -8,6 +8,9 @@ func _process(delta):
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.has_method("hit"):
+	if "hit" in body:
 		body.hit()
+	queue_free()
+
+func _on_timer_timeout() -> void:
 	queue_free()
