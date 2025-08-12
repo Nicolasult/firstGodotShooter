fdd89fcd5142fa80425121e0346c4284cdbb49b3
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-12 19:16:02 +0200

    scout setup

---
+++ scenes/enemies/scout.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+var player_nearby: bool = false
+var can_laser: bool = true
+
+signal laser(pos, direction)
+
+func _process(_delta):
+	if player_nearby:
+		look_at(Globals.player_pos)
+		if can_laser:
+			var pos: Vector2 = $LaserSpawnPositions/Marker2D.global_position
+			var direction: Vector2 = (Globals.player_pos - position).normalized()
+			laser.emit(pos, direction)
+			can_laser = false
+			$LaserCooldown.start()
+
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	player_nearby = true
+
+
+func _on_attack_area_body_exited(body: Node2D) -> void:
+	player_nearby = false
+
+
+func _on_laser_cooldown_timeout() -> void:
+	can_laser = true
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -8,6 +8,8 @@ var item_scene : PackedScene = preload("res://scenes/items/item.tscn")
 func _ready():
 	for container in get_tree().get_nodes_in_group("Container"):
 		container.connect("open", _on_container_opened)
+	for scout in get_tree().get_nodes_in_group("Scouts"):
+		scout.connect("laser", _on_scout_laser)
 	
 func _on_container_opened(pos, direction):
 	var item = item_scene.instantiate()
@@ -15,12 +17,15 @@ func _on_container_opened(pos, direction):
 	item.direction = direction
 	$Items.call_deferred("add_child", item)
 
-func _on_player_player_shot_laser(pos, direction) -> void:
+func create_laser(pos, direction):
 	var laser = laser_scene.instantiate() as Area2D
 	laser.position = pos
 	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
 	laser.direction = direction
 	$Projectiles.add_child(laser)
+
+func _on_player_player_shot_laser(pos, direction) -> void:
+	create_laser(pos, direction)
 
 
 func _on_player_player_shot_grenade(pos, direction) -> void:
@@ -29,6 +34,8 @@ func _on_player_player_shot_grenade(pos, direction) -> void:
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
 
+func _on_scout_laser(pos, direction):
+	create_laser(pos, direction)
 
 func _on_house_player_entered() -> void:
 	var tween = get_tree().create_tween()
