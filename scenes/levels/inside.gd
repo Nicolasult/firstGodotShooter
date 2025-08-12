94060b785bfb0d89400425d7e02f4a8905378d22
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-12 10:59:43 +0200

    add proper transition animation between levels

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -4,4 +4,4 @@ extends LevelParent
 func _on_south_door_body_entered(_body: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
-	get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
+	TransitionLayer.change_scene("res://scenes/levels/outside.tscn")
