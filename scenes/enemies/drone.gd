cb754a0c749936849fd589b4b2552050af352e8a
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-13 17:06:12 +0200

    drone refinement

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -1,10 +1,29 @@
 extends CharacterBody2D
 
+var active: bool = true
+var vulnerable: bool = true
+
+var speed: int = 400
+var health: int = 40
+
 func _process(_delta):
-	var direction = Vector2.RIGHT
-	velocity = direction * 100
-	move_and_slide()
+	if active:
+		look_at(Globals.player_pos)
+		var direction = (Globals.player_pos - position).normalized()
+		velocity = direction * speed
+		move_and_slide()
 	
 func hit():
-	print("damage")
+	if vulnerable:
+		health -= 10
+		vulnerable = false
+		$HitTimer.start()
+	if health <= 0:
+		queue_free()
 	
+func _on_notice_area_body_entered(_body: Node2D) -> void:
+	active = true
+
+
+func _on_hit_timer_timeout() -> void:
+	vulnerable = true
