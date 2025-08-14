b8b82393099523a5337ceef4b934e902cc8160cb
nicolasult <nicolas.ultrillo@gmail.com>
2025-08-14 15:30:27 +0200

    update turrets on the car

--- scenes/enemies/car.gd
+++ scenes/enemies/car.gd
@@ -5,8 +5,18 @@ var player_near: bool = false
 @onready var line1: Line2D = $Turret/RayCast2D/Line2D
 @onready var line2: Line2D = $Turret/RayCast2D2/Line2D
 
+@onready var gun_fire1: Sprite2D = $Turret/GunFire1
+@onready var gun_fire2: Sprite2D = $Turret/GunFire2
+
 func fire():
 	Globals.health -= 20
+	gun_fire1.modulate.a = 1
+	gun_fire2.modulate.a = 1
+	
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(gun_fire1, "modulate:a", 0, randf_range(0.1, 0.5))
+	tween.tween_property(gun_fire2, "modulate:a", 0, randf_range(0.1, 0.5))
 
 func _ready():
 	line1.add_point($Turret/RayCast2D.target_position)
